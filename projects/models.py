9bf54c34e53cfaa3222550ab268fac265c4ca2e4
nasriv <vnasri91@gmail.com>
2020-12-03 17:11:07 -0500

    added project model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Projects(models.Models):
+    image = models.ImageField(upload_to='images/')
+    descripton = models.TextField(max_length=255)
+    link = models.TextField(max_length=255)
