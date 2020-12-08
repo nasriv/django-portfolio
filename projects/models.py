fd4e0e0e2f10c4e5ef6e1228227ec7d0e00cd543
nasriv <vnasri91@gmail.com>
2020-12-08 00:48:13 -0500

    updated home.html with new template

--- projects/models.py
+++ projects/models.py
@@ -4,7 +4,7 @@ class Project(models.Model):
     # by default Django will create auto-increment id
     title = models.CharField(max_length=150)
     image = models.ImageField(upload_to='images/', blank=True)
-    descripton = models.TextField(max_length=255)
+    description = models.TextField(max_length=255)
     url = models.CharField(max_length=255, blank=True)
     date_built = models.DateField()
 
